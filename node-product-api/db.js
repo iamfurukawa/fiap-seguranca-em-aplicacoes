9fd2cd51eac27c97276f386a6f60b9747f3c736d
thiagom <tmalcher@positivo.com.br>
2023-05-14 21:21:51 -0400

    fixed db.js fixed db.js against code Injection by implementing input field validation and using prepared statements

--- node-product-api/db.js
+++ node-product-api/db.js
@@ -1,84 +1,90 @@
 const { randomUUID } = require('crypto');
+const mysql = require("mysql2/promise");
 
-async function connect(){
-    if(global.connection && global.connection.state !== 'disconnected')
-        return global.connection;
- 
-    const mysql = require("mysql2/promise");
-    const connection = await mysql.createConnection({
-        host: process.env.DB_HOST || 'localhost',
-        port: 3306,
-        user: 'test',
-        password: 'test',
-        database: 'finalProjectSubst',
-        multipleStatements: true
-      } );
-    console.log("Conectou no MySQL!");
-    global.connection = connection;
-    return connection;
+async function connect() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    port: 3306,
+    user: 'test',
+    password: 'test',
+    database: 'finalProjectSubst',
+    multipleStatements: true
+  });
+  console.log("Conectou no MySQL!");
+  return connection;
 }
 
-async function getAllProducts(){
+async function getAllProducts() {
+  const conn = await connect();
+
+  const query = `SELECT * FROM products LIMIT 1000;`;
+  console.log(`Executando query: ${query}`);
+
+  const [rows, fields] = await conn.execute(query);
+  console.log(`Rows: ${JSON.stringify(rows)}`);
+  conn.end();
+  return rows;
+}
+
+async function getProductById(id) {
+  const conn = await connect();
+
+  const query = `SELECT * FROM products WHERE id = ?;`;
+  console.log(`Executando query: ${query}`);
+
+  const [rows, fields] = await conn.execute(query, [id]);
+  conn.end();
+  return rows;
+}
+
+async function updateProductById(id, name, description, value) {
+  try {
     const conn = await connect();
-    
-    const query = `SELECT * FROM products LIMIT 1000;`;
+
+    const query = `UPDATE products SET name = ?, description = ?, value = ? WHERE id = ?;`;
     console.log(`Executando query: ${query}`);
 
-    const [rows, fields] = await connection.execute(query);
-    console.log(`Rows: ${JSON.stringify(rows)}`);
+    const [rows] = await conn.execute(query, [name, description, value, id]);
+    conn.end();
     return rows;
+  } catch (err) {
+    throw { code: 500, message: 'Erro inesperado ao tentar atualizar o produto' };
+  }
 }
 
-async function getProductById(id){
-    const conn = await connect();
-    
-    const query = `SELECT * FROM products WHERE id = "${id}";`;
-    console.log(`Executando query: ${query}`);
-    
-    const [rows, fields] = await connection.execute(query);
+async function deleteProductById(id) {
+  const conn = await connect();
 
-    return rows;
+  const query = `DELETE FROM products WHERE id = ?;`;
+  console.log(`Executando query: ${query}`);
+
+  await conn.execute(query, [id]);
+  conn.end();
 }
 
+async function insertProduct(name, description, value) {
+  const conn = await connect();
 
-async function updateProductById(id, name, description, value){
-    try{
-        const conn = await connect();
-    
-        const query = `UPDATE products SET name = "${name}", description = "${description}", value = ${value} WHERE id = "${id}";`;
-        console.log(`Executando query: ${query}`);
-        
-        const [rows] = await conn.execute(query);
-        return rows;
-    }catch(err){
-        throw {code: 500, message: 'Erro inesperado ao tentar cadastrar usuário'};
+  // Validação de entrada
+  if (!name || !description || !value) {
+    throw { code: 400, message: 'Todos os campos são obrigatórios' };
+  }
+
+  const id = randomUUID();
+  const query = `INSERT INTO products(id, name, description, value) VALUES (?, ?, ?, ?);`;
+  console.log(`Executando query: ${query}`);
+
+  try {
+    // Prepared statement
+    await conn.execute(query, [id, name, description, value]);
+    conn.end();
+  } catch (err) {
+    if (err.errno === 1062) {
+      throw { code: 400, message: 'Já existe um produto cadastrado com este nome!' };
+    } else {
+      throw { code: 500, message: 'Erro inesperado ao tentar cadastrar o produto' };
     }
+  }
 }
 
-async function deleteProductById(id){
-    const conn = await connect();
-    
-    const query = `DELETE FROM products WHERE id = "${id}";`;
-    console.log(`Executando query: ${query}`);
-
-    await connection.execute(query);
-}
-
-async function insertProduct(name, description, value){
-    const conn = await connect();
-
-    const query = `INSERT INTO products(id, name, description, value) VALUES ("${randomUUID()}", "${name}", "${description}", ${value});`;
-    console.log(`Executando query: ${query}`);
-
-    try{
-        await connection.execute(query);
-    }catch(err){
-        if(err.errno === 1062){
-            throw {code: 400, message: 'Já existe um producte cadastrado com este usuário!'};
-        }else{
-            throw {code: 500, message: 'Erro inesperado ao tentar cadastrar usuário'};
-        }
-    }
-}
-
-module.exports = {getProductById, getAllProducts, insertProduct, updateProductById, deleteProductById}
+module.exports = { getProductById, getAllProducts, insertProduct, updateProductById, deleteProductById }
